344dbf7873ebaff81a5f1494dc244f37eceb2111
lamboley <lucas.lamboley@outlook.com>
2026-09-21 20:41:12 +0200

    refactor: remove unused constants and simplify GetRole function

--- ForeverQoL_Utils.lua
+++ ForeverQoL_Utils.lua
@@ -1,17 +1,7 @@
 local ForeverQoL = select(2, ...)
 
-local CONST_ARMOR_CLASS = 4
-local CONST_LEATHER = 2
-local CONST_MAIL = 3
-local CONST_PLATE = 4
-local CONST_SHIELD = 6
-
 ---Role of the current specialization, nil on a character that has none yet
 function ForeverQoL.GetRole()
-	local currentSpec = GetSpecialization()
-	if not currentSpec then
-		return nil
-	end
-
-	return GetSpecializationRole(currentSpec)
+    local currentSpec = GetSpecialization()
+    return currentSpec and GetSpecializationRole(currentSpec)
 end
