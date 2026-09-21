6f97254eb89199bac94ab4af37228415d5ccfdeb
lamboley <lucas.lamboley@outlook.com>
2026-09-21 20:41:08 +0200

    refactor: simplify scale calculation in SetPerfectPixelScale function

--- ForeverQoL_System_Graphics.lua
+++ ForeverQoL_System_Graphics.lua
@@ -2,20 +2,17 @@ local ForeverQoL = select(2, ...)
 
 local Graphics = CreateFrame("Frame", "ForeverQoL_Graphics")
 
-local CONST_MINIMUM_SCALE = 0.4
-local CONST_MAXIMUM_SCALE = 1.15
+local CONST_MINIMUM_SCALE, CONST_MAXIMUM_SCALE = 0.4, 1.15
 
 function Graphics:SetPerfectPixelScale()
     local screenHeight = tonumber(ForeverQoLData.Configs["UseCustomHeight"]) or select(2, GetPhysicalScreenSize())
-    local scale = math.max(CONST_MINIMUM_SCALE, math.min(CONST_MAXIMUM_SCALE, 768 / screenHeight))
-    UIParent:SetScale(scale)
+    UIParent:SetScale(math.max(CONST_MINIMUM_SCALE, math.min(CONST_MAXIMUM_SCALE, 768 / screenHeight)))
 end
 
 function Graphics:Init()
     if ForeverQoLData.Configs["UsePerfectPixel"] then
         self:RegisterEvent("PLAYER_ENTERING_WORLD")
         self:RegisterEvent("DISPLAY_SIZE_CHANGED")
-
         self:SetScript("OnEvent", self.SetPerfectPixelScale)
     end
 end
